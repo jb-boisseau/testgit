19be85ef6493091d58a87df7d369bec2a5364ee6
jb <jbboisseau@gmail.com>
2017-11-09 09:34:21 +0100

    structure html : un message explicite

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+
+<body>
+    <h1>J'utilise Git</h1>
+    <div class="container">
+        <div class="row">
+            <img src="hyttoaoa.png">
+        </div>
+        
+    </div>
+</body>
+</html>
